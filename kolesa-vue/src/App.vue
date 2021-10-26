1496fdd47cfdafb7e0c2ea8a6b77b0970d5d996e
Ruslan <kupzhasarov14@gmail.com>
2021-10-26 18:55:37 +0600

    feat: add User component

--- kolesa-vue/src/App.vue
+++ kolesa-vue/src/App.vue
@@ -10,17 +10,7 @@
           />
           <div class="header__inner inner">
             <search></search>
-            <a href="#" class="inner__user user">
-              <img
-                src="../src/assets/img/morti.svg"
-                alt="user-photo"
-                class="user__img"
-              />
-              <div class="user__name-points">
-                <p class="user__name">Мортиджан</p>
-                <p class="user__point">300 баллов</p>
-              </div>
-            </a>
+            <user></user>
           </div>
         </div>
       </div>
@@ -224,12 +214,14 @@
 <script>
 import Modal from './components/Modal.vue';
 import Search from './components/Search.vue';
+import User from './components/User.vue';
 
 export default {
   name: 'App',
   components: {
     Modal,
     Search,
+    User,
   },
   data() {
     return {
---
+++ kolesa-vue/src/components/User.vue
@@ -0,0 +1,15 @@
+<template>
+  <a href="#" class="inner__user user">
+    <img src="@/assets/img/morti.svg" alt="user-photo" class="user__img" />
+    <div class="user__name-points">
+      <p class="user__name">Мортиджан</p>
+      <p class="user__point">300 баллов</p>
+    </div>
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'User',
+};
+</script>
